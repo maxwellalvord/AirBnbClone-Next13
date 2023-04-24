4c1c35ba4640b3a66a3d9977c7688599809110f0
Maxwell Alvord <maxwellalvord@gmail.com>
2023-04-24 13:15:39 -0700

    update: navbar usermenu UI

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { AiOutlineMenu } from "react-icons/ai"
+
 const UserMenu = () => {
   return (
     <div className='relative'>
@@ -20,6 +22,29 @@ const UserMenu = () => {
           '
         >
           Airbnb your home
+        </div>
+        <div
+          onClick={() => {}}
+          className="
+            p-4
+            md:py-1
+            md:px-2
+            border-[1px] 
+            border-neutral-200 
+            flex 
+            flex-row 
+            items-center 
+            gap-3 
+            rounded-full 
+            cursor-pointer 
+            hover:shadow-md 
+            transition
+          "
+        >
+          <AiOutlineMenu />
+          <div className="hidden md:block">
+            <Avatar />
+          </div>
         </div>
       </div>
     </div>
