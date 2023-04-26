87a89fb276f9f7a8d2ee483ca973a23be0ad9d69
Maxwell Alvord <maxwellalvord@gmail.com>
2023-04-26 11:38:18 -0700

    BUGFIX: Updated Next package to fix, Unexpected token u in JSON at position 0, bug.

---
+++ app/components/navbar/Categories.tsx
@@ -0,0 +1,70 @@
+
+'use client';
+
+import { usePathname, useSearchParams } from 'next/navigation';
+import { TbBeach, TbMountain, TbPool } from 'react-icons/tb';
+import { 
+  GiBarn, 
+  GiBoatFishing, 
+  GiCactus, 
+  GiCastle, 
+  GiCaveEntrance, 
+  GiForestCamp, 
+  GiIsland,
+  GiWindmill
+} from 'react-icons/gi';
+import { FaSkiing } from 'react-icons/fa';
+import { BsSnow } from 'react-icons/bs';
+import { IoDiamond } from 'react-icons/io5';
+import { MdOutlineVilla } from 'react-icons/md';
+
+
+import Container from '../Container';
+import CategoryBox from '../CategoryBox';
+
+
+export const categories = [
+  {
+    label: 'Beach',
+    icon: TbBeach,
+    description: 'This property is close to the beach!',
+  },
+  {
+    label: 'Windmills',
+    icon: GiWindmill,
+    description: 'This property is has windmills!',
+  },
+  {
+    label: 'Modern',
+    icon: MdOutlineVilla,
+    description: 'This property is modern!'
+  },
+]
+
+const Categories = () => {
+  return (
+    <Container>
+      <div
+        className="
+          pt-4
+          flex 
+          flex-row 
+          items-center 
+          justify-between
+          overflow-x-auto
+        "
+      >
+        {categories.map((item) => (
+          <CategoryBox 
+            key={item.label}
+            label={item.label}
+            description={item.description}
+            icon={item.icon}
+          />
+        ))}
+      </div>
+    </Container>
+  )
+}
+
+export default Categories
