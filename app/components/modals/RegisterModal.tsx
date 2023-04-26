0dc1d55b73a2c3bd99932e6440f29e1717ccd0dc
Maxwell Alvord <maxwellalvord@gmail.com>
2023-04-26 12:03:24 -0700

    Update: add final touches to user auth UI.

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -10,6 +10,7 @@ import {
 } from 'react-hook-form';
 
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
 import axios from 'axios';
 import Modal from './Modal';
 import Heading from "../Heading";
@@ -19,8 +20,10 @@ import Button from '../Button';
 import { signIn } from 'next-auth/react';
 
 
+
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
   const [isLoading, setIsLoading] = useState(false);
 
   const {
@@ -51,6 +54,11 @@ const RegisterModal = () => {
         setIsLoading(false);
       })
   }
+
+  const toggle = useCallback(() => {
+    registerModal.onClose();
+    loginModal.onOpen();
+  }, [loginModal, registerModal]);
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
@@ -114,7 +122,7 @@ const RegisterModal = () => {
             Already have an account?
           </div>
           <div
-          onClick={registerModal.onClose}
+          onClick={toggle}
             className='
               text-neutral-800
               cursor-pointer
