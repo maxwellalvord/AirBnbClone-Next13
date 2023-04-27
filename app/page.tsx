c0a56041aedabbaf249503e429239af24b684e2c
Maxwell Alvord <maxwellalvord@gmail.com>
2023-04-27 14:10:00 -0700

    BUGFIX: fix bug in prisma schema causing typing error for createAt.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import getListings from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
 
-export default function Home() {
-  const isEmpty = true;
+export default async function Home() {
+  const listings = await getListings();
 
-  if (isEmpty) {
+  if (listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState showReset />
