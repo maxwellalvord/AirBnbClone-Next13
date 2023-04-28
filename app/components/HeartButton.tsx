a6b44929f502a7274e3185d6fe0db6f16adca108
Maxwell Alvord <maxwellalvord@gmail.com>
2023-04-28 09:12:12 -0700

    WIP: building out the listings page/ listingCard component

---
+++ app/components/HeartButton.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+
+const HeartButton = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default HeartButton
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
+import getCurrentUser from "./actions/getCurrentUser";
 import getListings from "./actions/getListings";
+
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
 import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
 export default async function Home() {
   const listings = await getListings();
+  const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
     return (
@@ -27,7 +31,16 @@ export default async function Home() {
           sxl:grid-cols-6
           gap-8
         ">
-          <div>My future listings</div>
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard
+                currentUser={currentUser} 
+                key={listing.id}
+                data={listing}
+
+              />
+            )
+          })}
         </div>
       </Container>
     </ClientOnly>
