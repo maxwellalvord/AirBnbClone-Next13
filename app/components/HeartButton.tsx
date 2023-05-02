27d9b82a7b156bb857f25619b50835b506b8258d
Maxwell Alvord <maxwellalvord@gmail.com>
2023-05-02 09:00:01 -0700

    Update: finish HeartButton component

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
 
 interface HeartButtonProps {
@@ -11,8 +12,35 @@ const HeartButton:React.FC<HeartButtonProps> = ({
   listingId,
   currentUser,
 }) => {
+  const hasFavorited = false;
+  const toggleFavorite = () => {};
+
   return (
-    <div></div>
+    <div
+      onClick={toggleFavorite}
+      className="
+        relative
+        hover:opacity-80
+        transition
+        cursor-pointer
+      "
+    >
+      <AiOutlineHeart
+        size={28}
+        className="
+          fill-white
+          absolute
+          -top-[2px]
+          -right-[2px]
+        "
+      />
+      <AiFillHeart
+        size={24}
+        className={
+          hasFavorited ? 'fill-rose-500' : 'fill-neutral-500/70'
+        }
+      />
+    </div>
   )
 }
 
