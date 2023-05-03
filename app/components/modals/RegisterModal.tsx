3e31eca31a8d0b64c85be139d0c1a3242759161d
Maxwell Alvord <maxwellalvord@gmail.com>
2023-05-03 13:47:52 -0700

    Update & BUGFIX: Build out properties page and trouble shoot bug preventing load.

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -45,7 +45,9 @@ const RegisterModal = () => {
 
     axios.post('/api/register', data)
       .then(() => {
+        toast.success('Registration Successful!')
         registerModal.onClose();
+        loginModal.onOpen();
       })
       .catch((error) => {
         toast.error('Something went wrong')
