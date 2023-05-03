b7e3b4aa316a8f3e06e7159460ceb80603e36125
Maxwell Alvord <maxwellalvord@gmail.com>
2023-05-03 09:42:13 -0700

    Update & BUGFIX: fix props getting passed through ListingHead and build out the component on the listing page.

---
+++ app/listings/[listingId]/ListingClient.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { Reservation } from "@prisma/client";
+import { useMemo } from "react";
+
+import { SafeListing, SafeUser } from "@/app/types";
+import { categories } from "@/app/components/navbar/Categories";
+import Container from "@/app/components/Container";
+import ListingHead from "@/app/components/listings/ListingHead";
+
+
+interface ListingClientProps {
+  reservations?: Reservation[];
+  listing: SafeListing & {
+    user: SafeUser
+  };
+  currentUser?: SafeUser | null
+}
+
+const ListingClient: React.FC<ListingClientProps> = ({
+  currentUser,
+  listing
+}) => {
+  const category = useMemo(() => {
+    return categories.find((items) => 
+     items.label === listing.category);
+ }, [listing.category]);
+
+  return (
+    <Container>
+      <div className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col gap-6">
+          <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue= {listing.locationValue}
+            id={listing.id}
+            currentUser={currentUser}
+          />
+        </div>
+      </div>
+    </Container>
+  )
+}
+
+export default ListingClient;
